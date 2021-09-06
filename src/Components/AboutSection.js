009c18fc3f9f5409f4c8f444ac3c073fa3a7e825
theobayard <41492964+theobayard@users.noreply.github.com>
2021-09-06 15:26:16 -0600

    Updated about section

--- src/Components/AboutSection.js
+++ src/Components/AboutSection.js
@@ -4,7 +4,7 @@ import theoPhoto from '../pic_of_theo.JPG'
 import {Col, Row} from 'react-bootstrap'
 
 function AboutSection() {
-    const bodyText = "> I am a Student at Pitzer College pursuing a bachelor's degree in computer science through Harvey Mudd's CS program. Currently, I am on track to graduate in May of 2022. I specialize in creating applications that enable users to explore data. Since May 2020 I have been working at Harvey Mudd's WHISK lab on a web app that enables researchers to more easily create and interpret ML, specifically LDA, models and have loved every minute of developing it (more info below). I was raised in Boulder Colorado, and in my free time I like to ski, explore nature, learn about the natural world, and cultivate gourmet mushrooms."
+    const bodyText = "> I am a Student at Pitzer College pursuing a bachelor's degree in computer science through Harvey Mudd's CS program. Currently, I am on track to graduate in May of 2022. I specialize in creating applications that enable users to explore data. Since May 2020 I have been working at Harvey Mudd's WHISK lab on a web app that enables researchers to more easily create and interpret ML, specifically LDA, models and have loved every minute of developing it (more info below). I was raised in Boulder Colorado, and in my free time I like to ski, explore nature, learn about the natural world, and design and print puzzle boxes."
     
     return (
         <PageSection id={"about"} title={"About"}> 
